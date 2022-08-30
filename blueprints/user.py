f597aff2abc17cc3b2dd3f422952fa9259d9664d
Mr.White <100204965+stromcao@users.noreply.github.com>
2022-08-30 23:03:00 +0800

    修正了曹向前电脑无法访问的问题

--- blueprints/user.py
+++ blueprints/user.py
@@ -4,9 +4,9 @@ from flask import Blueprint,render_template
 bp = Blueprint("user",__name__,url_prefix="/user")
 
 @bp.route("/login")
-def user_login():
+def login():
     return render_template("Login.html")
 
 @bp.route("/register")
-def user_register():
+def register():
     return render_template("register.html")
