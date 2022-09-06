aab17a7ce30e3c5e51c0eb32814601af3674b293
Mr.White <100204965+stromcao@users.noreply.github.com>
2022-09-06 20:39:20 +0800

    第一次
    添加了邮箱验证功能，完善了登录功能，但还没有实现与后台数据库交互

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask
 import config
-from exts import db
+from exts import db,mail
 # 在此导入蓝图
 from blueprints import user_bp, video_bp
 
 app = Flask(__name__)
 app.config.from_object(config)
 db.init_app(app)
+mail.init_app(app)
 # 在此注册蓝图
 app.register_blueprint(user_bp)
 app.register_blueprint(video_bp)
--- blueprints/user.py
+++ blueprints/user.py
@@ -1,5 +1,6 @@
 from flask import Blueprint,render_template
-
+from exts import mail
+from flask_mail import Message
 #url_prefix:作为前缀 127.0.0.1:5000/user
 bp = Blueprint("user",__name__,url_prefix="/user")
 
@@ -10,3 +11,13 @@ def login():
 @bp.route("/register")
 def register():
     return render_template("register.html")
+
+@bp.route("/mail")
+def my_mail():
+    message = Message(
+        subject="邮箱测试",
+        recipients=['1252967009@qq.com'],
+        body="这是一篇测试邮件"
+    )
+    mail.send(message)
+    return "success"
