e1f8a6034368302f87a03ba63ea48c2367847054
lyj20011210 <74363871+lyj20011210@users.noreply.github.com>
2022-09-20 20:49:26 +0800

    随便放点数据到html玩玩

--- blueprints/register.py
+++ blueprints/register.py
@@ -1,8 +1,9 @@
 from flask import Blueprint,render_template
-
 bp = Blueprint("register",__name__,url_prefix="/register")
 
 
 @bp.route("/register")
 def register():
-    return render_template("register.html")
+    id=123
+    passwd=123
+    return render_template("register.html",id=id,passwd=passwd)
