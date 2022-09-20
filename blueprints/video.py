27d3fdc7e3a870ea2bb31d86da31aa0558713707
0xiansheng <78599051+0xiansheng@users.noreply.github.com>
2022-09-20 21:03:36 +0800

    21:03

--- blueprints/video.py
+++ blueprints/video.py
@@ -7,3 +7,7 @@ bp=Blueprint("video",__name__,url_prefix="/")
 @bp.route("/")
 def index():
     return render_template("index.html")
+
+@bp.route("/detail")
+def detail():
+    return render_template("detail.html")
