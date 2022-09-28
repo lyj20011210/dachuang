c41ee00a10cecf4a311de88902dd5441169c062d
lyj20011210 <74363871+lyj20011210@users.noreply.github.com>
2022-09-28 22:40:44 +0800

    实现了注册功能，并且完成了数据持久化

--- blueprints/register.py
+++ blueprints/register.py
@@ -1,9 +1,25 @@
-from flask import Blueprint,render_template
+from flask import Blueprint, render_template, request, redirect, url_for
 from Models import UserModel
-bp = Blueprint("register",__name__,url_prefix="/register")
+from .forms import RegisterForm
+from exts import db
+
+bp = Blueprint("register", __name__, url_prefix="/register")
 
 
-@bp.route("/register")
+@bp.route("/register", methods=["GET", "POST"])
 def register():
+    if request.method == 'GET':
+        return render_template("register.html")
+    elif request.method=='POST':
+        form = RegisterForm(request.form)
+        print(request.form['id'])
+        user = form.id.data
+        passwd1 = form.password1.data
+        passwd2 = form.password2.data
+        print(passwd2)
+        user = UserModel(username=user, password=passwd1)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for("login.login"))
 
-    return render_template("register.html")
+
