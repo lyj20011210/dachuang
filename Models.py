ef3954416481ae19f1d1923ea4b7e34469710a49
0xiansheng <78599051+0xiansheng@users.noreply.github.com>
2022-10-05 13:18:18 +0800

    创建一个表video——list，完成动态加载首页，动态绑定视频播放页，实现分页功能以及修改部分样式

--- Models.py
+++ Models.py
@@ -9,3 +9,13 @@ class UserModel(db.Model):
     username = db.Column(db.String(200), nullable=False, unique=True)
     password = db.Column(db.String(200), nullable=False)
     join_time = db.Column(db.DateTime, default=datetime.now)
+
+
+class Videos_List(db.Model):
+    __tablename__ = "videos_list"
+    video_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    video_name = db.Column(db.String(200))
+    video_link = db.Column(db.String(200))
+    video_view = db.Column(db.String(200))
+    video_comments = db.Column(db.String(200))
+    video_publishedtime = db.Column(db.String(200))
--- blueprints/video.py
+++ blueprints/video.py
@@ -1,15 +1,47 @@
-from flask import Blueprint, render_template, session
+from flask import Blueprint, render_template, session, request
+from flask_paginate import get_page_parameter, Pagination
 
-bp=Blueprint("video",__name__,url_prefix="/")
+import config
+from exts import db
+bp = Blueprint("video", __name__, url_prefix="/")
 
-
-
-@bp.route("/")
-def index():
-    user=session.get("name")
+@bp.route("/", defaults={'page': 1})
+@bp.route("/<page>")
+def index(page):
+    user = session.get("name")
     print(user)
-    return render_template("index.html",user=user)
+    str = "select * from video_list"
+
+    # 分页语句
+    limitpart = " LIMIT {limit} offset {offset} "
+
+    # 每页记录行数定为9
+    limit = 9
+
+    # 获取当前页码
+    page = request.args.get(get_page_parameter(), type=int, default=int(page))
+
+    # 判断当前行和偏移量
+    offset = (9 * int(page) - 9)
+
+    # 获取总页数
+    sqlcount = "select count(*) from ( "+ str+" )t1"
+    total = db.session.execute(sqlcount).fetchone()[0]
+
+    # 获取当前页的SQL语句
+    sql = str + limitpart
+    sql=sql.format(limit=limit, offset=offset)
+    video_list = db.session.execute(sql)
+
+    # 获取分页代码
+    paginate= Pagination(page=page, total=total, per_page=9)
+
+    return render_template("index.html", user=user, video_list=video_list, paginate=paginate)
+
 
 @bp.route("/detail")
 def detail():
-    return render_template("detail.html")
+    vid = request.args.get("vid")
+    sql = "select * from video_list where video_id="+vid
+    video_list = db.session.execute(sql)
+    return render_template("detail.html",video_list=video_list)
